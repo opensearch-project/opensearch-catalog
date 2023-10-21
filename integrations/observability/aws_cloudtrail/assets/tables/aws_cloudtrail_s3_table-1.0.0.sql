9bb7f6475d898b63d791db85024ff2a2d869fa76
YANGDB <yang.db.dev@gmail.com>
2023-10-21 11:24:45 -0700

    update the cloudtrail s3 samples
    Signed-off-by: YANGDB <yang.db.dev@gmail.com>

--- integrations/observability/aws_cloudtrail/assets/tables/aws_cloudtrail_s3_table-1.0.0.sql
+++ integrations/observability/aws_cloudtrail/assets/tables/aws_cloudtrail_s3_table-1.0.0.sql
@@ -1,38 +1,55 @@
-CREATE TABLE ${table}
-(
-    eventVersion                 STRING,
-    userIdentity                 STRUCT<
-        type : STRING,
-    principalId: STRING,
-    arn: STRING,
-    accountId: STRING,
-    accessKeyId: STRING,
-    userName: STRING,
-    sessionContext: STRUCT< attributes: STRUCT<
-        creationDate: STRING,
-    mfaAuthenticated: STRING > >
+CREATE EXTERNAL TABLE IF NOT EXISTS ${table} (
+    Records ARRAY<STRUCT<
+        eventVersion: STRING,
+        userIdentity: STRUCT<
+            type: STRING,
+            principalId: STRING,
+            arn: STRING,
+            accountId: STRING,
+            accessKeyId: STRING,
+            userName: STRING,
+            sessionContext: STRUCT<
+                sessionIssuer: STRUCT<>,
+                webIdFederationData: STRUCT<>,
+                attributes: STRUCT<
+                    creationDate: STRING,
+                    mfaAuthenticated: STRING
+                >
+            >
         >,
-    eventTime                    STRING,
-    eventSource                  STRING,
-    eventName                    STRING,
-    awsRegion                    STRING,
-    sourceIPAddress              STRING,
-    userAgent                    STRING,
-    requestParameters            STRUCT<
-        name : STRING
+        eventTime: STRING,
+        eventSource: STRING,
+        eventName: STRING,
+        awsRegion: STRING,
+        sourceIPAddress: STRING,
+        userAgent: STRING,
+        requestParameters: STRUCT<
+            roleName: STRING,
+            description: STRING,
+            assumeRolePolicyDocument: STRING
         >,
-    responseElements             STRING,
-    requestID                    STRING,
-    eventID                      STRING,
-    readOnly                     BOOLEAN,
-    eventType                    STRING,
-    managementEvent              BOOLEAN,
-    recipientAccountId           STRING,
-    eventCategory                STRING,
-    tlsDetails                   STRUCT< tlsVersion: STRING,
-    cipherSuite: STRING,
-    clientProvidedHostHeader: STRING >,
-    sessionCredentialFromConsole STRING
-) USING json OPTIONS (
-  'path' ${bucket}
-);
+        responseElements: STRUCT<
+            role: STRUCT<
+                assumeRolePolicyDocument: STRING,
+                arn: STRING,
+                roleId: STRING,
+                createDate: STRING,
+                roleName: STRING,
+                path: STRING
+            >
+        >,
+        requestID: STRING,
+        eventID: STRING,
+        readOnly: BOOLEAN,
+        eventType: STRING,
+        managementEvent: BOOLEAN,
+        recipientAccountId: STRING,
+        eventCategory: STRING,
+        tlsDetails: STRUCT<
+            tlsVersion: STRING,
+            cipherSuite: STRING,
+            clientProvidedHostHeader: STRING
+        >,
+        sessionCredentialFromConsole: STRING
+    >>
+) USING json OPTIONS ('path' ${bucket});
