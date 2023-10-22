d87d0b8ea21d255e413d4f0f794c8e363e7c0df3
YANGDB <yang.db.dev@gmail.com>
2023-10-21 22:45:24 -0700

    update the cloudtrail s3 DDL statement
    Signed-off-by: YANGDB <yang.db.dev@gmail.com>

--- integrations/observability/aws_cloudtrail/assets/tables/aws_cloudtrail_s3_table-1.0.0.sql
+++ integrations/observability/aws_cloudtrail/assets/tables/aws_cloudtrail_s3_table-1.0.0.sql
@@ -1,55 +1,53 @@
 CREATE EXTERNAL TABLE IF NOT EXISTS ${table} (
     Records ARRAY<STRUCT<
+        awsRegion: STRING,
+        eventCategory: STRING,
+        eventID: STRING,
+        eventName: STRING,
+        eventSource: STRING,
+        eventTime: STRING,
+        eventType: STRING,
         eventVersion: STRING,
+        managementEvent: STRING,
+        readOnly: BOOLEAN,
+        recipientAccountId: STRING,
+        requestID: STRING,
+        requestParameters: STRUCT<
+            assumeRolePolicyDocument: STRING,
+            description: STRING,
+            roleName: STRING
+        >,
+        responseElements: STRUCT<
+            role: STRUCT<
+                arn: STRING,
+                assumeRolePolicyDocument: STRING,
+                createDate: STRING,
+                path: STRING,
+                roleId: STRING,
+                roleName: STRING
+                >
+        >,
+        sessionCredentialFromConsole: STRING,
+        sourceIPAddress: STRING,
+        tlsDetails: STRUCT<
+            cipherSuite: STRING,
+            clientProvidedHostHeader: STRING,
+            tlsVersion: STRING
+        >,
+        userAgent: STRING,
         userIdentity: STRUCT<
-            type: STRING,
-            principalId: STRING,
-            arn: STRING,
-            accountId: STRING,
             accessKeyId: STRING,
-            userName: STRING,
+            accountId: STRING,
+            arn: STRING,
+            principalId: STRING,
             sessionContext: STRUCT<
-                sessionIssuer: STRUCT<>,
-                webIdFederationData: STRUCT<>,
                 attributes: STRUCT<
                     creationDate: STRING,
                     mfaAuthenticated: STRING
                 >
-            >
-        >,
-        eventTime: STRING,
-        eventSource: STRING,
-        eventName: STRING,
-        awsRegion: STRING,
-        sourceIPAddress: STRING,
-        userAgent: STRING,
-        requestParameters: STRUCT<
-            roleName: STRING,
-            description: STRING,
-            assumeRolePolicyDocument: STRING
-        >,
-        responseElements: STRUCT<
-            role: STRUCT<
-                assumeRolePolicyDocument: STRING,
-                arn: STRING,
-                roleId: STRING,
-                createDate: STRING,
-                roleName: STRING,
-                path: STRING
-            >
-        >,
-        requestID: STRING,
-        eventID: STRING,
-        readOnly: BOOLEAN,
-        eventType: STRING,
-        managementEvent: BOOLEAN,
-        recipientAccountId: STRING,
-        eventCategory: STRING,
-        tlsDetails: STRUCT<
-            tlsVersion: STRING,
-            cipherSuite: STRING,
-            clientProvidedHostHeader: STRING
-        >,
-        sessionCredentialFromConsole: STRING
+            >,
+            type: STRING,
+            userName: STRING
+        >
     >>
 ) USING json OPTIONS ('path' ${bucket});
