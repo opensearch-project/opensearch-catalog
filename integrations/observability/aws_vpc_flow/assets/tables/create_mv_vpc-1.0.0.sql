f4c2ccf557d5a1ad48bdb720cf018680e48735cb
YANGDB <yang.db.dev@gmail.com>
2023-10-30 16:51:57 -0700

    append additional custom vpc flow log fields
    Signed-off-by: YANGDB <yang.db.dev@gmail.com>

--- integrations/observability/aws_vpc_flow/assets/tables/create_mv_vpc-1.0.0.sql
+++ integrations/observability/aws_vpc_flow/assets/tables/create_mv_vpc-1.0.0.sql
@@ -13,6 +13,20 @@ CREATE MATERIALIZED VIEW {table_name}_mview AS
         FROM_UNIXTIME(start) as `@timestamp`,
         FROM_UNIXTIME(end) as `aws.vpc.end`,
         action as `aws.vpc.action`,
-        log_status as `aws.vpc.log-status`
+        log_status as `aws.vpc.log-status`,
+        vpc_id as `aws.vpc.vpc-id`,
+        subnet_id as `aws.vpc.subnet-id`,
+        instance_id as `aws.vpc.instance-id`,
+        region as `aws.vpc.region`,
+        az_id as `aws.vpc.az-id`,
+        sublocation_type as `aws.vpc.sublocation-type`,
+        sublocation_id as `aws.vpc.sublocation-id`,
+        tcp_flags as `aws.vpc.tcp-flags`,
+        pkt_srcaddr as `aws.vpc.pkt-srcaddr`,
+        pkt_dstaddr as `aws.vpc.pkt-dstaddr`,
+        pkt_src_aws_service as `aws.vpc.pkt-src-aws-service`,
+        pkt_dst_aws_service as `aws.vpc.pkt-dst-aws-service`,
+        traffic_path as `aws.vpc.traffic-path`,
+        flow_direction as `aws.vpc.flow-direction`
 FROM
     {table_name};
